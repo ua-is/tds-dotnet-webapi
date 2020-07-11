c2b54cd5e81149747ea5d8f765a4159acbab3b0e
Juan Flores-Moroco <ajaflorez.upc@gmail.com>
2020-07-10 21:32:16 -0500

    first commit

---
+++ WebApiCovid/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiCovid.Entities;
+
+namespace WebApiCovid.Contexts
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+        }
+        // Defino la relacion entre tabla y entity
+        public DbSet<Medico> Medicos { get; set; }
+    }
+}
---
+++ WebApiCovid/Entities/Medico.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiCovid.Entities
+{
+    public class Medico
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Dni { get; set; }
+        public string Especialidad { get; set; }
+
+    }
+}
