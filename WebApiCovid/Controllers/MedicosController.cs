472fc90508f135dcc564256940b7316274448b5c
Juan Flores-Moroco <ajaflorez.upc@gmail.com>
2020-07-10 22:07:23 -0500

    Added MIgration and COntroller

---
+++ WebApiCovid/Controllers/MedicosController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiCovid.Contexts;
+using WebApiCovid.Entities;
+
+namespace WebApiCovid.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MedicosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Medicos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Medico>>> GetMedicos()
+        {
+            return await _context.Medicos.ToListAsync();
+        }
+
+        // GET: api/Medicos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Medico>> GetMedico(int id)
+        {
+            var medico = await _context.Medicos.FindAsync(id);
+
+            if (medico == null)
+            {
+                return NotFound();
+            }
+
+            return medico;
+        }
+
+        // PUT: api/Medicos/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMedico(int id, Medico medico)
+        {
+            if (id != medico.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(medico).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MedicoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Medicos
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Medico>> PostMedico(Medico medico)
+        {
+            _context.Medicos.Add(medico);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMedico", new { id = medico.Id }, medico);
+        }
+
+        // DELETE: api/Medicos/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Medico>> DeleteMedico(int id)
+        {
+            var medico = await _context.Medicos.FindAsync(id);
+            if (medico == null)
+            {
+                return NotFound();
+            }
+
+            _context.Medicos.Remove(medico);
+            await _context.SaveChangesAsync();
+
+            return medico;
+        }
+
+        private bool MedicoExists(int id)
+        {
+            return _context.Medicos.Any(e => e.Id == id);
+        }
+    }
+}
